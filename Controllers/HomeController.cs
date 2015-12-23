37c5b0b992f87e1ee3c09644d2da3dfd8074ae1a
bjoerntx <bjoern@textcontrol.com>
2015-12-23 15:05:34 +0100

    Uploaded sample

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tx_insertTableDropDown.Models;
+
+namespace tx_insertTableDropDown.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // this method inserts a new table to a temporary
+        // ServerTextControl and returns the complete table
+        [HttpPost]
+        public string GetTableFromController(Table model)
+        {
+            byte[] data;
+
+            using (TXTextControl.ServerTextControl tx =
+                new TXTextControl.ServerTextControl())
+            {
+                tx.Create();
+                tx.Tables.Add(model.Rows, model.Columns);
+
+                tx.Save(out data, TXTextControl.BinaryStreamType.InternalUnicodeFormat);
+            }
+
+            return Convert.ToBase64String(data);
+        }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tx_insertTableDropDown.Models
+{
+    public class Table
+    {
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+    }
+}
